491930e80ed37a2ef8617245a87404223b6ca698
c.danyl <ch.danyl@gmail.com>
2015-04-21 22:17:24 +0200

    Add more unit test

---
+++ readme.md
@@ -0,0 +1,7 @@
+Unit Test
+-------------
+The test framework of choice for this mock server is Mocha:
+http://visionmedia.github.io/mocha/
+ 1. along with Supertest for HTTP assertions https://github.com/visionmedia/supertest
+ 2. and Should.js for BDD style tests https://github.com/visionmedia/should.js
+Mocha is a great choice because it makes async test natural and fluent.
--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 var restify = require('restify');
+var mongojs = require("mongojs");
 var Config = require('../config');
+var should = require('should');
 
 // init the test client
 var client = restify.createJsonClient({
@@ -7,23 +9,89 @@ var client = restify.createJsonClient({
     url: 'http://localhost:3000'
 });
 
-describe('service: hello', function () {
+describe('Routing', function () {
 
-    // Test #1
-    describe('200 response check', function () {
+    before(function (done) {
+        // Mongo db connection
+        var connection_string = Config.DB_SERVER + ':' + Config.DB_PORT + '/' + Config.DB_NAME;
+        mongojs.connect(connection_string);
+        done();
+    });
+
+    describe('service: getUsers', function () {
+
+        //it('listen and close (port only)', function (t) {
+        //    var server = restify.createServer();
+        //    server.listen(4673, function () {
+        //        server.close();
+        //        });
+        //    });
+
+        //before(function() {
+        //    server = restify.createServer();
+        //    server.listen(4242);
+        //});
+        //
+        //after(function(){
+        //    server.close();
+        //});
+
         it('should get a 200 response', function (done) {
             client.get('/userSettings/getUsers', function (err, req, res, data) {
                 if (err) {
                     throw new Error(err);
                 }
                 else {
-                    if (data.code != 200) {
-                        throw new Error('invalid response from /userSettings/getUsers');
+                    if (res.code == 200) {
+                        throw new Error('valid response from /userSettings/getUsers');
                     }
                     done();
                 }
             });
         });
+
+        //it('should Post a new user', function (done) {
+        //    var user = {
+        //        name: 'test name',
+        //        msd: '1234',
+        //        value: {'tt': 'tt'},
+        //        postedOn: new Date()
+        //    };
+        //
+        //    client
+        //        .post('/userSettings')
+        //        .send(201, user)
+        //        .end(function (err, res) {
+        //            if (err) {
+        //                throw err;
+        //            }
+        //            // this is should.js syntax, very clear
+        //            res.should.have.status(201);
+        //            done();
+        //        });
+        //});
+
+        describe('200 response check', function() {
+            it('should get a 200 response', function(done) {
+                    var user = {
+                        name: 'test name',
+                        msd: '1234',
+                        value: {'tt': 'tt'},
+                        postedOn: new Date()
+                    };
+                client.post('/userSettings', user, function(err, req, res, data) {
+                    if (err) {
+                        throw new Error(err);
+                    }
+                    else {
+
+                        if (data.code != 201) {
+                            throw new Error('valid response from /post');
+                        }
+                        done();
+                    }
+                });
+            });
+        });
     });
-    // Add more tests as needed...
 });
